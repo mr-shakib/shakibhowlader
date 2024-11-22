7b13c053b35a1686027bde30296d5ed911f306d0
Shakib <132547096+mr-shakib@users.noreply.github.com>
2024-11-23 02:13:55 +0600

    Update HeroSection.tsx

--- src/components/hero/HeroSection.tsx
+++ src/components/hero/HeroSection.tsx
@@ -79,7 +79,7 @@ export default function HeroSection() {
                 </button>
 
                 <a
-                  href="/Tariq_Naser.pdf"
+                  href="/Shakib.pdf"
                   download
                   className="px-8 py-3 bg-transparent border-2 border-indigo-500 text-indigo-500 font-bold rounded-full hover:bg-indigo-500 hover:text-white transition-all duration-300 text-center transform hover:-translate-y-1"
                   aria-label="Download CV"
